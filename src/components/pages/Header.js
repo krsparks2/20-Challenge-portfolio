caf849c7a1b55797f274abd1af0f3a333bdc4e7c
Keli <krsparks2@yahoo.com>
2022-09-25 11:58:01 -0700

    initial commit

---
+++ src/components/pages/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navigation from "./components/Navigation";
+
+// const Header = () => <Navigation />;
+
+export default function Header() {
+  return (
+    <div>
+      <h1>Header</h1>
+      <p>
+        header content
+      </p>
+    </div>
+  );
+}
+
